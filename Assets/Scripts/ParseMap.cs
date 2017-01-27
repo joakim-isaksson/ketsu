f9c928fc90bf69aa112234697737ab1698af957a
Essi Jukkala <essi.jukkala@aalto.fi>
2017-01-27 23:13:38 +0200

    first version of map parser

---
+++ Assets/Scripts/ParseMap.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using System.Text.RegularExpressions;
+
+[ExecuteInEditMode]
+public class ParseMap : MonoBehaviour {
+	//map structure to hold information
+	public class Map {
+		public int height;
+    	public int width;
+    	public int amountOfTiles;
+    	public int[,] tiles;
+	}
+
+	public Transform GroundTest;
+	public Transform WaterTest;
+	public Transform SandTest;
+	public Transform RockTest;
+
+	#if UNITY_EDITOR
+    [SerializeField]
+    private bool initialized = false;
+ 
+    void Awake() { Initialize(); }
+ 
+    void Initialize()
+    {
+        if (initialized)
+        	return;
+ 
+		//init new map
+		Map map = new Map();
+		string tileString = "";
+
+		//read json file
+		string line = null;
+		int n = 0;
+		//want to save height, width and the tile numbers
+		List<int> save = new List<int> {4,5,10};
+
+		//which map to read, make this smarter later
+		StreamReader reader = new StreamReader("./Assets/Maps/ParserTestMap/TestMap.json");
+		while ((line = reader.ReadLine()) != null) {
+	        n++;
+
+	        //if we want to check the line or not
+	        if (!save.Contains(n))
+	            continue;
+
+	        //parse the variables
+			int start = line.IndexOf(":", 0)+1;
+        	int end = line.IndexOf(",", start);
+
+	        if(line.Contains("height"))
+	        	int.TryParse(line.Substring(start, end - start), out map.height);
+	        if(line.Contains("width"))
+	        	int.TryParse(line.Substring(start, end - start), out map.width);
+	        if(line.Contains("data"))
+	        	tileString = line.Substring(start+1, line.LastIndexOf(',')-2-start);
+
+	        map.amountOfTiles = map.width * map.height;
+	    }
+		//parse the string of tiles to an actual array
+	    map.tiles = parseTiles(tileString, map.width, map.height);
+
+	    //instantiate GameObjects via prefabs
+	    createObjects(map.tiles, map.width, map.height);
+ 
+        initialized = true;
+	}
+
+	int[,] parseTiles(string tileString, int x, int y){
+		tileString = Regex.Replace(tileString, @"\s+", "");
+		string[] substrings = tileString.Split(',');
+
+		int[,] tiles = new int[y, x];
+		for(int i=0; i<y; i++) {
+			for(int j=0; j<x; j++){
+				int.TryParse(substrings[i*y+j], out tiles[i,j]);
+			}
+		}
+		return tiles;
+	}
+
+	void createObjects(int[,] tiles, int x, int y){
+		for(int i=0; i<y; i++){
+			for(int j=0; j<x; j++){
+				if(tiles[i,j]==2)
+					Instantiate(GroundTest, new Vector3(-j+x, 0, -i+y), Quaternion.identity);
+				if(tiles[i,j]==1)
+					Instantiate(RockTest, new Vector3(-j+x, 0, -i+y), Quaternion.identity);
+				if(tiles[i,j]==3)
+					Instantiate(SandTest, new Vector3(-j+x, 0, -i+y), Quaternion.identity);
+				if(tiles[i,j]==4)
+					Instantiate(WaterTest, new Vector3(-j+x, 0, -i+y), Quaternion.identity);
+			}
+		}
+	}
+	#endif
+
+}
+
