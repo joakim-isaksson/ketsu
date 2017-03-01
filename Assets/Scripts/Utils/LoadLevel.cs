e74d255e3911949633bd8e400e12e2db3bfbf9c6
jaine <essi.jukkala@gmail.com>
2017-03-01 22:34:20 +0200

    next level button

--- Assets/Scripts/Utils/LoadLevel.cs
+++ Assets/Scripts/Utils/LoadLevel.cs
@@ -8,4 +8,9 @@ public class LoadLevel : MonoBehaviour {
 	public void LoadOn(int level){
 		SceneManager.LoadScene(level);
 	}
+
+	public void NextLevel(){
+		 int level = SceneManager.GetActiveScene().buildIndex;
+		 SceneManager.LoadScene(level+1);
+	}
 }
