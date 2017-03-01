ba6e3a090685de6a534b67d3aac095afb568ff3f
Joakim Isaksson <joakim.ove.isaksson@gmail.com>
2017-03-01 20:50:03 +0200

    sheeeep has not brains

--- Assets/Scripts/Game/Sheep.cs
+++ Assets/Scripts/Game/Sheep.cs
@@ -11,6 +11,7 @@ namespace Ketsu.Game
         public Transform Pumber;
 
         Map map;
+        Vector3 target;
 
         void Awake()
         {
@@ -22,18 +23,50 @@ namespace Ketsu.Game
             UpdatePositionFromWorld();
 
             map = MapManager.LoadedMap;
+
+            SetNewTarget();
         }
 
         void Update()
         {
             // Move forward
-            transform.Translate(Vector3.forward * MoveSpeed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, target, MoveSpeed * Time.deltaTime);
+            if (Vector3.Distance(transform.position, target) < 0.001f)
+            {
+                transform.position = target;
+                SetNewTarget();
+            }
 
             // If pumber is hitting something -> turn
             IntVector2 pumberPos = IntVector2.FromXZ(Pumber.position);
             if (!map.Contains(pumberPos) || Blocked(pumberPos) != null)
             {
-                transform.Rotate(Vector3.up, 90.0f);
+                SetNewTarget();
+            }
+        }
+
+        void SetNewTarget()
+        {
+            List<Vector3> directions = new List<Vector3>();
+            directions.Add(transform.forward);
+            directions.Add(-transform.right);
+            directions.Add(transform.right);
+            directions.Add(-transform.forward);
+            directions.Add(Vector3.zero);
+
+            foreach (Vector3 direction in directions)
+            {
+                IntVector2 targetPos = IntVector2.FromXZ(transform.position + direction);
+                if (map.Contains(targetPos) && Blocked(targetPos) == null)
+                {
+                    target = new Vector3(
+                        targetPos.X,
+                        transform.position.y,
+                        targetPos.Y
+                    );
+                    transform.LookAt(target);
+                    return;
+                }
             }
         }
 
@@ -41,7 +74,7 @@ namespace Ketsu.Game
         {
             foreach (MapObject obj in map.GetObjects(point))
             {
-                if (obj != this) continue;
+                if (obj == this) continue;
                 if (obj.Type == MapObjectType.Water) return obj;
                 if (obj.Layer != MapLayer.Ground) return obj;
             }
