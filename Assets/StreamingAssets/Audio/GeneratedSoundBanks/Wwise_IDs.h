8404c90aaabee40c02e6a100afc8360c619826b4
Mikko Kolehmainen <mikko.kolehmainen@gmail.com>
2017-03-04 13:51:56 +0200

    UI Menu Button sounds added

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -21,12 +21,30 @@ namespace AK
         static const AkUniqueID DEATH_WOLF_HEDGEHOG = 3738416424U;
         static const AkUniqueID DEATH_WOLF_WATER = 1446448900U;
         static const AkUniqueID LEVEL_WIN = 2908556826U;
+        static const AkUniqueID LEVELMENU_LOCKEDLEVEL_BUTTON = 3773943064U;
+        static const AkUniqueID LEVELMENU_UNLOCKEDLEVEL_BUTTON = 1513439813U;
         static const AkUniqueID MOVE_FOX = 2492116432U;
         static const AkUniqueID MOVE_HEDGEHOG = 941274754U;
         static const AkUniqueID MOVE_KETSU = 3609283987U;
         static const AkUniqueID MOVE_WOLF = 4249145201U;
+        static const AkUniqueID POPOPMENU_SETTINGS_BUTTON = 323011021U;
+        static const AkUniqueID POPUPMENU_BACK_BUTTON = 3094295669U;
+        static const AkUniqueID POPUPMENU_MAINMENU_BUTTON = 3215618068U;
+        static const AkUniqueID POPUPMENU_RESTART_BUTTON = 3455221783U;
+        static const AkUniqueID POPUPMENU_STORE_BUTTON = 2656457985U;
+        static const AkUniqueID STARTMENU_CREDITS_BUTTON = 1952017332U;
+        static const AkUniqueID STARTMENU_LOGO_BUTTON = 2533867219U;
+        static const AkUniqueID STARTMENU_SETTINGS_BUTTON = 3791509005U;
+        static const AkUniqueID STARTMENU_START_BUTTON = 1575699130U;
+        static const AkUniqueID STARTMENU_STORE_BUTTON = 1415003611U;
         static const AkUniqueID TRANSFORM_FROMKETSU = 1374719650U;
         static const AkUniqueID TRANSFORM_TOKETSU = 1052828255U;
+        static const AkUniqueID WINSCREEN_MAINMENU_BUTTON = 1670207177U;
+        static const AkUniqueID WINSCREEN_NEXT_BUTTON = 2660933722U;
+        static const AkUniqueID WINSCREEN_RESTART_BUTTON = 171788004U;
+        static const AkUniqueID WINSCREEN_STORE_BUTTON = 482381986U;
+        static const AkUniqueID WORLDMENU_LOCKEDWORLD_BUTTON = 376222116U;
+        static const AkUniqueID WORLDMENU_UNLOCKEDWORLD_BUTTON = 574974477U;
     } // namespace EVENTS
 
     namespace BANKS
