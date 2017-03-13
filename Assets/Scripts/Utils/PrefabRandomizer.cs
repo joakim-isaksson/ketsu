373c0314f66616af7f3a3bb9b6c8cbea1fdc2fde
Essi Jukkala <essi.jukkala@aalto.fi>
2017-03-13 15:50:32 +0200

    randomize bush and tree size and position

---
+++ Assets/Scripts/Utils/PrefabRandomizer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ketsu.Game;
+
+public class PrefabRandomizer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if(gameObject.GetComponent<MapObject>().Type == MapObjectType.Tree ||
+			gameObject.GetComponent<MapObject>().Type == MapObjectType.Bush){
+			float rnd = Random.Range(0.8f, 1.2f);
+			gameObject.transform.localScale = new Vector3(rnd, rnd, rnd);
+			Vector3 pos = gameObject.transform.position;
+			gameObject.transform.position = new Vector3(pos.x+Random.Range(0.0f, 0.2f), pos.y, pos.z+Random.Range(0.0f, 0.2f));
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
