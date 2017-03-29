fefb31d0b12ba223b3eb9549f6ac21538fc3b6bb
jaine <essi.jukkala@gmail.com>
2017-03-29 23:12:25 +0300

    level selection can haz objects to click to enter level

---
+++ Assets/Scripts/UI/LevelSelection.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LevelSelection : MonoBehaviour {
+	public int level;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseDown(){
+        // this object was clicked - do something
+        SceneManager.LoadScene(level+1);
+  	}  
+}
