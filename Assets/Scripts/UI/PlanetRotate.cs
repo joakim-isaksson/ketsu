7d209e1a4d5af57f7c93d0b16a4c84f31bb6089a
jaine <essi.jukkala@gmail.com>
2017-03-29 22:57:38 +0300

    level selection planet rotates

---
+++ Assets/Scripts/UI/PlanetRotate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Ketsu.UI
+{
+public class PlanetRotate : MonoBehaviour {
+	float rotationSpeed = 5.0f;
+ 
+ 	Vector3 speed = new Vector3();
+ 	bool dragging = false;
+ 
+	void OnMouseOver() 
+	{
+	    dragging = true;
+	}
+ 
+ 	void Update () 
+ 	{
+     	if (Input.GetMouseButton(0) && dragging) {
+         	speed = new Vector3(-Input.GetAxis ("Mouse X"), Input.GetAxis("Mouse Y"), 0);
+     	} else {
+         	if (dragging) {
+             	speed = Vector3.zero;
+             	dragging = false;
+         	}
+    	}
+    	transform.Rotate(speed.y * rotationSpeed, speed.x * rotationSpeed, 0, Space.World);
+ 	}
+}
+
+}
