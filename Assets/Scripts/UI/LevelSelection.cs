e5c4788958d3f0c7ddc8fde2ab5b3fd6ea4f9256
jaine <essi.jukkala@gmail.com>
2017-03-30 20:47:12 +0300

    testing difference in win and mac

--- Assets/Scripts/UI/LevelSelection.cs
+++ Assets/Scripts/UI/LevelSelection.cs
@@ -16,17 +16,18 @@ public class LevelSelection : MonoBehaviour {
   	}  
 
   	IEnumerator Fly(){
-		for(var t = 0f; t < 1; t += Time.deltaTime) {
+		for(float t = 0f; t < 1; t += Time.deltaTime) {
             spaceship.transform.rotation = Quaternion.Lerp(spaceship.transform.rotation, transform.rotation, t);
             yield return null;
          }
   	}
 
   	IEnumerator Load(){
-		for(var t = 0f; t < 1; t += Time.deltaTime) {
+		for(float t = 0f; t < 1; t += Time.deltaTime) {
             spaceship.transform.position = Vector3.Lerp(spaceship.transform.position, new Vector3(spaceship.transform.position.x, spaceship.transform.position.y, spaceship.transform.position.z+0.04f), t);
             yield return null;
         }
+        Debug.Log(spaceship.transform.position);
   		SceneManager.LoadScene(level+1);
   		yield return null;
   	}
