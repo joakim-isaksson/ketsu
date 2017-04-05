6bd893aeb132e32b35096e8582e38b34b7bb5136
Mikko Kolehmainen <mikko.kolehmainen@gmail.com>
2017-04-05 13:41:02 +0300

    Wwise soundbanks updated and Unity InGame_Soundbank load changed

---
+++ Ketsu_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,139 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID CHARACTER_HIT_HEDGEHOG = 4257406112U;
+        static const AkUniqueID CHARACTER_HIT_WATER = 287526684U;
+        static const AkUniqueID CHICKEN_HIT_BUSH = 1746299433U;
+        static const AkUniqueID COLLECT_CHICKEN = 1004812793U;
+        static const AkUniqueID COLLECT_SHEEP = 30088657U;
+        static const AkUniqueID DEATH_FOX_HEDGEHOG = 4193159569U;
+        static const AkUniqueID DEATH_FOX_WATER = 269859599U;
+        static const AkUniqueID DEATH_WOLF_HEDGEHOG = 3738416424U;
+        static const AkUniqueID DEATH_WOLF_WATER = 1446448900U;
+        static const AkUniqueID GUI_CHICKEN_BUTTON = 1203244949U;
+        static const AkUniqueID GUI_KETSUPOWER_INCREASE = 4100043833U;
+        static const AkUniqueID GUI_MAXPOWERUP_CRYSTAL_BUTTON = 1464712323U;
+        static const AkUniqueID GUI_POWERUP_CRYSTAL_BUTTON = 4091529515U;
+        static const AkUniqueID GUI_SETTINGS_BUTTON = 989405037U;
+        static const AkUniqueID GUI_SHEEP_BUTTON = 3275066385U;
+        static const AkUniqueID GUI_SPLIT_ACTIVE = 841337824U;
+        static const AkUniqueID GUI_SPLIT_RELEASE = 1748211077U;
+        static const AkUniqueID LEVEL_WIN = 2908556826U;
+        static const AkUniqueID LEVELMENU_LOCKEDLEVEL_SELECT = 2297784124U;
+        static const AkUniqueID LEVELMENU_STARTLEVEL_SELECT = 2355422256U;
+        static const AkUniqueID LEVELMENU_UNLOCKEDLEVEL_SELECT = 2207195397U;
+        static const AkUniqueID MAP_ENV_BEES = 240649815U;
+        static const AkUniqueID MAP_ENV_BIGWATER = 611079635U;
+        static const AkUniqueID MAP_ENV_BIRDS = 35994456U;
+        static const AkUniqueID MAP_ENV_MEDIUMWIND = 4112863043U;
+        static const AkUniqueID MAP_ENV_SEA = 1320716757U;
+        static const AkUniqueID MAP_ENV_SMALLWATER = 782833506U;
+        static const AkUniqueID MAP_ENV_STRONGWIND = 1833815307U;
+        static const AkUniqueID MOVE_FOX = 2492116432U;
+        static const AkUniqueID MOVE_FOX_ICE = 4045630760U;
+        static const AkUniqueID MOVE_HEDGEHOG = 941274754U;
+        static const AkUniqueID MOVE_KETSU = 3609283987U;
+        static const AkUniqueID MOVE_WOLF = 4249145201U;
+        static const AkUniqueID MOVE_WOLF_ICE = 1426970985U;
+        static const AkUniqueID PLAY_INGAMEMUSIC_HEDGY = 620526404U;
+        static const AkUniqueID PLAY_MENUMUSIC = 2570041344U;
+        static const AkUniqueID PLAY_RANDOM_ENV = 2431936319U;
+        static const AkUniqueID PLAY_TUTORIALMUSIC = 2086825057U;
+        static const AkUniqueID POPOPMENU_SETTINGS_BUTTON = 323011021U;
+        static const AkUniqueID POPUPMENU_BACK_BUTTON = 3094295669U;
+        static const AkUniqueID POPUPMENU_MAINMENU_BUTTON = 3215618068U;
+        static const AkUniqueID POPUPMENU_RESTART_BUTTON = 3455221783U;
+        static const AkUniqueID POPUPMENU_STORE_BUTTON = 2656457985U;
+        static const AkUniqueID STARTMENU_CREDITS_BUTTON = 1952017332U;
+        static const AkUniqueID STARTMENU_LOGO_BUTTON = 2533867219U;
+        static const AkUniqueID STARTMENU_SETTINGS_BUTTON = 3791509005U;
+        static const AkUniqueID STARTMENU_START_BUTTON = 1575699130U;
+        static const AkUniqueID STARTMENU_STORE_BUTTON = 1415003611U;
+        static const AkUniqueID STOP_FOX_ICE = 2494762103U;
+        static const AkUniqueID STOP_WOLF_ICE = 3853446416U;
+        static const AkUniqueID TOOLBOX_COLLECT_COINS = 4162518518U;
+        static const AkUniqueID TOOLBOX_COLLECT_KETSUMAXPOWERUP = 1024872546U;
+        static const AkUniqueID TOOLBOX_COLLECT_KETSUPOWERUP = 2696066024U;
+        static const AkUniqueID TOOLBOX_COLLECT_SPACESHIPPART = 3565206619U;
+        static const AkUniqueID TOOLBOX_OPEN = 1173277773U;
+        static const AkUniqueID TRANSFORM_FROMKETSU = 1374719650U;
+        static const AkUniqueID TRANSFORM_TOKETSU = 1052828255U;
+        static const AkUniqueID UI_SPACESHIP_IDLE = 4062746263U;
+        static const AkUniqueID WINSCREEN_MAINMENU_BUTTON = 1670207177U;
+        static const AkUniqueID WINSCREEN_NEXT_BUTTON = 2660933722U;
+        static const AkUniqueID WINSCREEN_RESTART_BUTTON = 171788004U;
+        static const AkUniqueID WINSCREEN_STORE_BUTTON = 482381986U;
+        static const AkUniqueID WORLDMENU_LOCKEDWORLD_BUTTON = 376222116U;
+        static const AkUniqueID WORLDMENU_UNLOCKEDWORLD_BUTTON = 574974477U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace LEVEL
+        {
+            static const AkUniqueID GROUP = 2782712965U;
+
+            namespace STATE
+            {
+                static const AkUniqueID LEVEL1 = 2678230382U;
+                static const AkUniqueID LEVEL2 = 2678230381U;
+                static const AkUniqueID LEVEL3 = 2678230380U;
+                static const AkUniqueID LEVEL4 = 2678230379U;
+                static const AkUniqueID LEVEL5 = 2678230378U;
+                static const AkUniqueID LEVEL6 = 2678230377U;
+                static const AkUniqueID LEVEL7 = 2678230376U;
+            } // namespace STATE
+        } // namespace LEVEL
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace TILE_MATERIAL
+        {
+            static const AkUniqueID GROUP = 253806231U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID BRIDGE = 2068062714U;
+                static const AkUniqueID GRASS = 4248645337U;
+                static const AkUniqueID GROUND = 2528658256U;
+                static const AkUniqueID ICE = 344481046U;
+                static const AkUniqueID MUD = 712897245U;
+            } // namespace SWITCH
+        } // namespace TILE_MATERIAL
+
+    } // namespace SWITCHES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID INGAME_SOUNDBANK = 4063213932U;
+        static const AkUniqueID MENU_SOUNDBANK = 1146882678U;
+        static const AkUniqueID MUSIC_SOUNDBANK = 3589812408U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
+        static const AkUniqueID UI = 1551306167U;
+    } // namespace BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
