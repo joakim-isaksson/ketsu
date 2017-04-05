7ab02be2a796dc08f895ce73c6ba52d9666292e5
Essi Jukkala <essi.jukkala@aalto.fi>
2017-04-05 09:19:36 +0300

    added button script

---
+++ Assets/Scripts/UI/ButtonScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour {
+
+	[Header("Sounds")]
+	public string ButtonSound;
+
+	// Use this for initialization
+	void Start () {
+		Button btn = gameObject.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick(){
+		AkSoundEngine.PostEvent(ButtonSound, gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
