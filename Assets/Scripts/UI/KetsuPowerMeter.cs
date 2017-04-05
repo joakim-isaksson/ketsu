5bf4ac58b9b3732cecb853523b8ac55c6bcba581
Essi Jukkala <essi.jukkala@aalto.fi>
2017-04-05 23:02:05 +0300

    implemented ketsu power meter to control panel

--- Assets/Scripts/UI/KetsuPowerMeter.cs
+++ Assets/Scripts/UI/KetsuPowerMeter.cs
@@ -6,22 +6,39 @@ namespace Ketsu.UI
 {
     public class KetsuPowerMeter : MonoBehaviour
     {
-        Text ketsuPowerText;
+        public Text ketsuPowerText;
         CharacterHandler charHandler;
+
+        [Header("PowerBar")]
+        public Component BG;
+        Image[] Colors;
 
         void Awake()
         {
-            ketsuPowerText = GetComponent<Text>();
+
         }
 
         void Start()
         {
             charHandler = FindObjectOfType<CharacterHandler>();
+            Colors = BG.GetComponentsInChildren<Image>();
+            for (int i=0; i<Colors.Length-1; i++)
+        	{
+        		if(i > charHandler.KetsuPower)
+        			Colors[i].enabled = false;
+        	}
         }
 
         void Update()
         {
             ketsuPowerText.text = charHandler.KetsuPower.ToString("0");
+            for (int i=0; i<Colors.Length-1; i++)
+        	{
+        		if(i > charHandler.KetsuPower)
+        			Colors[i].enabled = false;
+        		else
+        			Colors[i].enabled = true;
+        	}
         }
     }
 }
