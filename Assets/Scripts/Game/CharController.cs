302368f39bd8c352900bbe0ed1d1c9ac4bc46bf0
jaine <essi.jukkala@gmail.com>
2017-04-11 19:30:14 +0300

    back to tapping on all devices

--- Assets/Scripts/Game/CharController.cs
+++ Assets/Scripts/Game/CharController.cs
@@ -31,7 +31,6 @@ namespace Ketsu.Game
 		{
             charHandler = FindObjectOfType<CharacterHandler>();
             mapManager = FindObjectOfType<MapManager>();
-            UseSwiping = true;
         }
 
 		void Update()
