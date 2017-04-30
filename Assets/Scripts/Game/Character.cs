71fdf68fc2c409ed5eff87db1fb04154b708f8cb
Joakim Isaksson <joakim.ove.isaksson@gmail.com>
2017-04-30 20:31:27 +0300

    char move sounds

--- Assets/Scripts/Game/Character.cs
+++ Assets/Scripts/Game/Character.cs
@@ -12,6 +12,9 @@ namespace Game
 
 		[Header("Sounds")]
 		public string SfxMove;
+	    public string SfxSlideStart;
+	    public string SfxSlideStop;
+        public string SwitchGroupName;
 
         bool stuckInMud;
 
@@ -24,41 +27,39 @@ namespace Game
 
         void Start()
 		{
-
-		}
+		    
+        }
 
 		public void MoveTo(Vector3 newPosition, MapObjectType targetGroundType, Action callback)
 		{
             if (stuckInMud || transform.position == newPosition)
             {
                 stuckInMud = false;
+
+                if (Type != MapObjectType.Ketsu) AkSoundEngine.SetSwitch(SwitchGroupName, targetGroundType.ToString(), gameObject);
+                if (targetGroundType != MapObjectType.Ice) AkSoundEngine.PostEvent(SfxMove, gameObject);
+
                 if (callback != null) callback();
             }
             else
             {
                 if (targetGroundType == MapObjectType.Mud) stuckInMud = true;
 
-                AkSoundEngine.PostEvent(SfxMove, gameObject);
-                MoveAnimation(newPosition, callback);
+                if (Type != MapObjectType.Ketsu) AkSoundEngine.SetSwitch(SwitchGroupName, targetGroundType.ToString(), gameObject);
+                if (targetGroundType != MapObjectType.Ice) AkSoundEngine.PostEvent(SfxMove, gameObject);
+
+                bool sliding = targetGroundType != MapObjectType.Ice;
+                StartCoroutine(RunMoveAnimation(newPosition, sliding, callback));
             }
 		}
 
-        public void TakeDamage(float amount)
+		IEnumerator RunMoveAnimation(Vector3 target, bool sliding, Action callback)
 		{
-            // do nothing
-        }
+		    if (sliding) AkSoundEngine.PostEvent(SfxSlideStart, gameObject);
 
-		void MoveAnimation(Vector3 target, Action callback)
-		{
-			StartCoroutine(RunMoveAnimation(target, callback));
-		}
-
-		IEnumerator RunMoveAnimation(Vector3 target, Action callback)
-		{
-		    anim.SetBool("Walking", true);
+            anim.SetBool("Walking", true);
 
             Vector3 start = transform.position;
-
             float animTime = MoveAnimTime * Vector3.Distance(start, target);
 
             transform.LookAt(target);
@@ -75,6 +76,8 @@ namespace Game
 			} while (timePassed < animTime);
 
             anim.SetBool("Walking", false);
+
+		    if (sliding) AkSoundEngine.PostEvent(SfxSlideStop, gameObject);
 
             if (callback != null) callback();
 		}
