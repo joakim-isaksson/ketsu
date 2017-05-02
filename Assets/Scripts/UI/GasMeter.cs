ce28ff0a150406144fa52d2679497e3843a447f2
jaine <essi.jukkala@gmail.com>
2017-05-02 19:34:02 +0300

    gas meter updated

--- Assets/Scripts/UI/GasMeter.cs
+++ Assets/Scripts/UI/GasMeter.cs
@@ -6,22 +6,20 @@ namespace Ketsu.UI
 {
     public class GasMeter : MonoBehaviour
     {
-        CharacterHandler charHandler;
         public Component Hand;
-
-        void Awake()
-        {
-
-        }
+        int StartGas = 0;
 
         void Start()
         {
-            charHandler = FindObjectOfType<CharacterHandler>();
+            StartGas = Gas.GasToCollect;
         }
 
         void Update()
         {
-
+        	if(Gas.GasToCollect!=0)
+        	    Hand.transform.rotation = Quaternion.Euler(0,0,(1.0f/StartGas*180)*(Gas.GasToCollect-StartGas)+90);
+            else 
+                Hand.transform.rotation = Quaternion.Euler(0,0,-90);
         }
     }
 }
