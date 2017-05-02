da2576c588d2c1a20a2c454b5a9d956a0c552ff8
jaine <essi.jukkala@gmail.com>
2017-05-02 14:56:34 +0300

    Game UI update started

---
+++ Assets/Scripts/UI/GasMeter.cs
@@ -0,0 +1,27 @@
+﻿using Game;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Ketsu.UI
+{
+    public class GasMeter : MonoBehaviour
+    {
+        CharacterHandler charHandler;
+        public Component Hand;
+
+        void Awake()
+        {
+
+        }
+
+        void Start()
+        {
+            charHandler = FindObjectOfType<CharacterHandler>();
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
