81914e9844688d6d088a78f7535b33b368697f19
Joakim Isaksson <joakim.ove.isaksson@gmail.com>
2017-05-04 00:20:01 +0300

    mud splatter

---
+++ Assets/Scripts/Game/Mud.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    public class Mud : MonoBehaviour
+    {
+        public GameObject SplatterPrefab;
+
+        void OnTriggerEnter(Collider other)
+        {
+            MapObject obj = other.GetComponent<MapObject>();
+            if (obj != null && (obj.Type == MapObjectType.Fox || obj.Type == MapObjectType.Wolf || obj.Type == MapObjectType.Ketsu))
+            {
+                Instantiate(SplatterPrefab, transform.position, transform.rotation);
+            }
+        }
+    }
+}
