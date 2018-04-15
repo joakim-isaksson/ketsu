4bb0653a53331b4a339f3a71ac2acfa6723080e7
Joakim Isaksson <joakim.ove.isaksson@gmail.com>
2018-04-15 20:52:07 +0300

    added blinking start text to start screen

--- Assets/Scripts/UI/LevelSelection.cs
+++ Assets/Scripts/UI/LevelSelection.cs
@@ -6,83 +6,101 @@ using UnityEngine.SceneManagement;
 
 public class LevelSelection : MonoBehaviour
 {
-    public int level;
-    public string LevelMusicEvent;
-    string sfxMapEnv = "Play_Map_Env";
+	public int level;
+	public string LevelMusicEvent;
+	string sfxMapEnv = "Play_Map_Env";
 
-    public static int currentLevel = 1;
+	public static int currentLevel = 1;
 	public GameObject spaceship;
 	public GameObject currentBase;
 	public GameObject Light;
-    Renderer rend;
-    Renderer lightrend;
-    public bool important;
-    Color StartColor = new Color(0.898f, 0.729f, 0.000f, 1.000f);
-    Color EndColor = new Color(0.898f, 0.451f, 0.000f, 1.000f);
-    bool animating;
+	Renderer rend;
+	Renderer lightrend;
+	public bool important;
+	Color StartColor = new Color (0.898f, 0.729f, 0.000f, 1.000f);
+	Color EndColor = new Color (0.898f, 0.451f, 0.000f, 1.000f);
+	bool animating;
 
-	void Start(){
-		rend = GetComponent<Renderer>();
-		currentBase = GameObject.Find("Level"+currentLevel);
-		if(spaceship.transform.rotation != currentBase.transform.rotation)
-		    StartCoroutine(FlyToBase(currentBase));
-		lightrend = Light.GetComponent<Renderer>();
-		lightrend.enabled=false;
+	private static bool firstVisit = true;
+
+	void Start ()
+	{
+		rend = GetComponent<Renderer> ();
+		currentBase = GameObject.Find ("Level" + currentLevel);
+		lightrend = Light.GetComponent<Renderer> ();
+		lightrend.enabled = false;
+
+		if (firstVisit) {
+			StartCoroutine (Load ());
+			return;
+		}
+
+		if (spaceship.transform.rotation != currentBase.transform.rotation)
+			StartCoroutine (FlyToBase (currentBase));
 	}
 
-	void OnMouseDown()
+	void OnMouseDown ()
 	{
-	    if (animating) return;
+		if (animating)
+			return;
 
-		if(spaceship.transform.rotation == transform.rotation)
-			StartCoroutine(Load());
+		if (spaceship.transform.rotation == transform.rotation)
+			StartCoroutine (Load ());
 		else
-        	StartCoroutine(Fly());
-  	}  
-
-  	IEnumerator Fly()
-	{
-	    animating = true;
-  		AkSoundEngine.PostEvent("LevelMenu_UnlockedLevel_Select", spaceship);
-		for(float t = 0f; t < 1; t += Time.deltaTime) {
-            spaceship.transform.rotation = Quaternion.Lerp(spaceship.transform.rotation, transform.rotation, t);
-            yield return null;
-        }
-	    spaceship.transform.rotation = transform.rotation;
-	    animating = false;
+			StartCoroutine (Fly ());
 	}
 
-  	IEnumerator FlyToBase(GameObject Base)
-    {
-	    animating = true;
-        for (float t = 0f; t < 1; t += Time.deltaTime) {
-            spaceship.transform.rotation = Quaternion.Lerp(spaceship.transform.rotation, Base.transform.rotation, t);
-            yield return null;
-        }
-        spaceship.transform.rotation = Base.transform.rotation;
-        animating = false;
-    }
+	IEnumerator FirstVisit ()
+	{
+		animating = true;
+		firstVisit = false;
+		yield return new WaitForSeconds (2);
+		StartCoroutine (Load ());
+	}
 
-  	IEnumerator Load()
-    {
-		lightrend.enabled=true;
-  		AkSoundEngine.PostEvent("LevelMenu_StartLevel_Select", spaceship);
-		for(float t = 0f; t < 1; t += Time.deltaTime) {
-            spaceship.transform.position = Vector3.Lerp(spaceship.transform.position, new Vector3(spaceship.transform.position.x, spaceship.transform.position.y, spaceship.transform.position.z+0.03f), t);
-            yield return null;
-        }
+	IEnumerator Fly ()
+	{
+		animating = true;
+		AkSoundEngine.PostEvent ("LevelMenu_UnlockedLevel_Select", spaceship);
+		for (float t = 0f; t < 1; t += Time.deltaTime) {
+			spaceship.transform.rotation = Quaternion.Lerp (spaceship.transform.rotation, transform.rotation, t);
+			yield return null;
+		}
+		spaceship.transform.rotation = transform.rotation;
+		animating = false;
+	}
 
-	    AkSoundEngine.PostEvent(LevelMusicEvent, gameObject);
-	    AkSoundEngine.PostEvent(sfxMapEnv, gameObject);
+	IEnumerator FlyToBase (GameObject Base)
+	{
+		animating = true;
+		for (float t = 0f; t < 1; t += Time.deltaTime) {
+			spaceship.transform.rotation = Quaternion.Lerp (spaceship.transform.rotation, Base.transform.rotation, t);
+			yield return null;
+		}
+		spaceship.transform.rotation = Base.transform.rotation;
+		animating = false;
+	}
 
-        currentLevel = level+1;
-  		SceneManager.LoadScene(level+1);
-  		yield return null;
-  	}
+	IEnumerator Load ()
+	{
+		lightrend.enabled = true;
+		AkSoundEngine.PostEvent ("LevelMenu_StartLevel_Select", spaceship);
+		for (float t = 0f; t < 1; t += Time.deltaTime) {
+			spaceship.transform.position = Vector3.Lerp (spaceship.transform.position, new Vector3 (spaceship.transform.position.x, spaceship.transform.position.y, spaceship.transform.position.z + 0.03f), t);
+			yield return null;
+		}
 
-  	void Update()
-    {
-  		if(important)
-        	rend.material.color = Color.Lerp(StartColor, EndColor, Mathf.PingPong(Time.time, 1));
-  	}
+		AkSoundEngine.PostEvent (LevelMusicEvent, gameObject);
+		AkSoundEngine.PostEvent (sfxMapEnv, gameObject);
+
+		currentLevel = level + 1;
+		SceneManager.LoadScene (level + 1);
+		yield return null;
+	}
+
+	void Update ()
+	{
+		if (important)
+			rend.material.color = Color.Lerp (StartColor, EndColor, Mathf.PingPong (Time.time, 1));
+	}
 }
