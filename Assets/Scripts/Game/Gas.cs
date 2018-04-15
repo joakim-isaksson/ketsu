7828eff1bddc31eb5264f099bf757fd4e359b92d
Joakim Isaksson <joakim.ove.isaksson@gmail.com>
2018-04-15 22:53:01 +0300

    instantly gain gem and gas resources when collected

--- Assets/Scripts/Game/Gas.cs
+++ Assets/Scripts/Game/Gas.cs
@@ -4,63 +4,66 @@ using System.Collections;
 
 namespace Game
 {
-    public class Gas : MonoBehaviour
-    {
-        public string SfxCollectDefault;
-        public string SfxCollectLast;
+	public class Gas : MonoBehaviour
+	{
+		public string SfxCollectDefault;
+		public string SfxCollectLast;
 
-        public static int GasToCollect;
-        public static int GasCollected;
+		public static int GasToCollect;
+		public static int GasCollected;
 
-        public GameObject ExplosionPrefab;
+		public GameObject ExplosionPrefab;
 
-        void Awake()
-        {
-            GasToCollect = 0;
-            GasCollected = 0;
-        }
+		bool Collected;
 
-        void Start()
-        {
-            GasToCollect++;
-        }
+		void Awake ()
+		{
+			GasToCollect = 0;
+			GasCollected = 0;
+		}
 
-        void OnTriggerEnter(Collider other)
-        {
-            var otherType = other.GetComponent<MapObject>().Type;
-            if (otherType != MapObjectType.Fox && otherType != MapObjectType.Wolf &&
-                otherType != MapObjectType.Ketsu) return;
+		void Start ()
+		{
+			GasToCollect++;
+		}
 
-            if (GasToCollect > 0) AkSoundEngine.PostEvent(SfxCollectDefault, gameObject);
-                else AkSoundEngine.PostEvent(SfxCollectLast, gameObject);
+		void OnTriggerEnter (Collider other)
+		{
+			if (Collected)
+				return;
+			Collected = true;
 
-            Instantiate(ExplosionPrefab, transform.position, transform.rotation);
+			var otherType = other.GetComponent<MapObject> ().Type;
+			if (otherType != MapObjectType.Fox && otherType != MapObjectType.Wolf &&
+			    otherType != MapObjectType.Ketsu)
+				return;
 
-            if(SceneManager.GetActiveScene().buildIndex - 1 < 4)
-                StartCoroutine(lerpPos(transform.position, new Vector3(9f,23f,8f), 1f));
-            else{
-                GasCollected++;
-                GasToCollect--;
+			if (GasToCollect > 0)
+				AkSoundEngine.PostEvent (SfxCollectDefault, gameObject);
+			else
+				AkSoundEngine.PostEvent (SfxCollectLast, gameObject);
 
-                Destroy(gameObject);
-            }
-        }
+			Instantiate (ExplosionPrefab, transform.position, transform.rotation);
 
-        IEnumerator lerpPos(Vector3 start, Vector3 end, float t){
-            float StartTime = Time.time;
-            while(Time.time < StartTime+t){
-                float timeProgressed = (Time.time - StartTime) / t;
-                float x = Mathf.Lerp(start.x, end.x, timeProgressed);
-                float y = Mathf.Lerp(start.y, end.y, timeProgressed);
-                float z = Mathf.Lerp(start.z, end.z, timeProgressed);
-                transform.position = new Vector3(x,y,z);
-                yield return new WaitForFixedUpdate();
-            }
-            GasCollected++;
-            GasToCollect--;
-            Instantiate(ExplosionPrefab, transform.position, transform.rotation);
-            Destroy(gameObject);
+			GasCollected++;
+			GasToCollect--;
 
-        }
-    }
+			StartCoroutine (lerpPos (transform.position, new Vector3 (9f, 23f, 8f), 0.6f));
+		}
+
+		IEnumerator lerpPos (Vector3 start, Vector3 end, float t)
+		{
+			float StartTime = Time.time;
+			while (Time.time < StartTime + t) {
+				float timeProgressed = (Time.time - StartTime) / t;
+				float x = Mathf.Lerp (start.x, end.x, timeProgressed);
+				float y = Mathf.Lerp (start.y, end.y, timeProgressed);
+				float z = Mathf.Lerp (start.z, end.z, timeProgressed);
+				transform.position = new Vector3 (x, y, z);
+				yield return new WaitForFixedUpdate ();
+			}
+			Instantiate (ExplosionPrefab, transform.position, transform.rotation);
+			Destroy (gameObject);
+		}
+	}
 }
--- Assets/Scripts/Game/Gem.cs
+++ Assets/Scripts/Game/Gem.cs
@@ -4,50 +4,52 @@ using System.Collections;
 
 namespace Game
 {
-    public class Gem : MonoBehaviour
-    {
-        public GameObject ExplosionPrefab;
-        public string SfxCollect;
+	public class Gem : MonoBehaviour
+	{
+		public GameObject ExplosionPrefab;
+		public string SfxCollect;
 
-        CharacterHandler characterHandler;
+		CharacterHandler characterHandler;
 
-        void Start()
-        {
-            characterHandler = FindObjectOfType<CharacterHandler>();
-        }
+		private bool Collected;
 
-        void OnTriggerEnter(Collider other)
-        {
-            var otherType = other.GetComponent<MapObject>().Type;
-            if (otherType != MapObjectType.Fox && otherType != MapObjectType.Wolf &&
-                otherType != MapObjectType.Ketsu) return;
+		void Start ()
+		{
+			characterHandler = FindObjectOfType<CharacterHandler> ();
+		}
 
-            AkSoundEngine.PostEvent(SfxCollect, gameObject);
+		void OnTriggerEnter (Collider other)
+		{
+			if (Collected)
+				return;
+			Collected = true;
 
-            Instantiate(ExplosionPrefab, transform.position, transform.rotation);
+			var otherType = other.GetComponent<MapObject> ().Type;
+			if (otherType != MapObjectType.Fox && otherType != MapObjectType.Wolf &&
+			             otherType != MapObjectType.Ketsu)
+				return;
 
-            if(SceneManager.GetActiveScene().buildIndex - 1 < 7)
-                StartCoroutine(lerpPos(transform.position, new Vector3(8f,23f,8f), 0.7f));
-            else{
-                characterHandler.FillKetsuPower();
-                Destroy(gameObject);
-            }
-        }
+			AkSoundEngine.PostEvent (SfxCollect, gameObject);
 
-        IEnumerator lerpPos(Vector3 start, Vector3 end, float t){
-            float StartTime = Time.time;
-            while(Time.time < StartTime+t){
-                float timeProgressed = (Time.time - StartTime) / t;
-                float x = Mathf.Lerp(start.x, end.x, timeProgressed);
-                float y = Mathf.Lerp(start.y, end.y, timeProgressed);
-                float z = Mathf.Lerp(start.z, end.z, timeProgressed);
-                transform.position = new Vector3(x,y,z);
-                yield return new WaitForFixedUpdate();
-            }
-            characterHandler.FillKetsuPower();
-            Instantiate(ExplosionPrefab, transform.position, transform.rotation);
-            Destroy(gameObject);
+			Instantiate (ExplosionPrefab, transform.position, transform.rotation);
 
-        }
-    }
+			StartCoroutine (lerpPos (transform.position, new Vector3 (8f, 23f, 8f), 0.6f));
+		}
+
+		IEnumerator lerpPos (Vector3 start, Vector3 end, float t)
+		{
+			float StartTime = Time.time;
+			while (Time.time < StartTime + t) {
+				float timeProgressed = (Time.time - StartTime) / t;
+				float x = Mathf.Lerp (start.x, end.x, timeProgressed);
+				float y = Mathf.Lerp (start.y, end.y, timeProgressed);
+				float z = Mathf.Lerp (start.z, end.z, timeProgressed);
+				transform.position = new Vector3 (x, y, z);
+				yield return new WaitForFixedUpdate ();
+			}
+			characterHandler.FillKetsuPower ();
+			Instantiate (ExplosionPrefab, transform.position, transform.rotation);
+			Destroy (gameObject);
+		}
+	}
 }
